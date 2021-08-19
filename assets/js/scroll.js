354333de68e876164c363c794413e8f64c770f96
Ramsblack <ramsblack@gmail.com>
2021-08-18 21:43:23 -0500

    subiendo de nuevo e portafolio

---
+++ assets/js/scroll.js
@@ -0,0 +1,17 @@
+const header = document.getElementById("main-header");
+
+function scrollHeaderTop() {
+  if(window.scrollY > 350) {
+      header.classList.add("fixed-header")
+  } else if (window.scrollY === 0) {
+    header.classList.remove("fixed-header");
+  }
+
+  if (window.scrollY > offsetAbout) {
+    about.style.opacity = "1";
+  }
+}
+
+
+
+document.addEventListener("scroll", scrollHeaderTop);
